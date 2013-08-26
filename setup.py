ff8f0c6ea90ac4f6d7e5a9d778cab506662ae786
Oliver Tonnhofer <olt@bogosoft.com>
2013-08-26 16:29:42 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='corsa',
-    version="0.1.0",
+    version="0.1.1",
     description='CORS proxy and web server for static apps',
     author='Oliver Tonnhofer',
     author_email='olt@bogosoft.com',
